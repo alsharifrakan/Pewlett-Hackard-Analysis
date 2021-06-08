217682914cd864cb7e67e2d8574c837f4deb2c96
alsharif rakan sadik <alsharif.rakan@gmail.com>
2021-06-08 15:58:03 -0400

    deliverable 1 and 2

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,42 @@
+SELECT e.emp_no,
+e.first_name,
+e.last_name,
+t.title,
+t.from_date,
+t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+select * from retirement_titles
+
+SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
+rt.first_name,
+rt.last_name,
+rt.title
+
+INTO unique_titles
+FROM retirement_titles AS rt
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT COUNT(title), title 
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
+
+SELECT DISTINCT ON (em.emp_no) em.emp_no, em.first_name, em.last_name, em.birth_date, de.from_date, de.to_date, ti.title
+INTO mentorship_elegibility 
+FROM employees AS em
+LEFT JOIN dept_emp AS de
+ON em.emp_no = de.emp_no
+LEFT JOIN titles AS ti
+ON em.emp_no = ti.emp_no
+WHERE (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY em.emp_no;
+
+select * from mentorship_elegibility;
